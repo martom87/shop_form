5c6e79b97554e9a04ab87152c98926c4d547b1b6
martom87 <marcintomala87@gmail.com>
2019-06-22 23:33:30 +0200

    working update service improved

--- app/services/order_items/update_service.rb
+++ app/services/order_items/update_service.rb
@@ -3,17 +3,16 @@
 module OrderItems
   class UpdateService < BaseService
     def call
-#      update
+      update
+    end
+
+    private
+
+    def update
       order_item = current_order.order_items.find(params[:id])
       order_item_params = params.require(:order_item).permit(:quantity, :product_id)
       order_item.update(order_item_params)
     end
-  end
 
-  private
-
-  def update
-    order_item = current_order.order_items.find(params[:id])
-    order_item.update(order_item_params)
   end
 end
