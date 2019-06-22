98f8dd8069fc07c7e1a7687bc44366421d7216b2
martom87 <marcintomala87@gmail.com>
2019-06-22 23:09:37 +0200

    service_preperation

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class OrderItemsController < ApplicationController
-  before_action :set_current_order, only: %i[create update destroy]
   before_action :set_order_items, only: %i[update destroy]
 
   def create
-    order_item = @order.order_items.new(order_item_params)
-    if @order.save
-      @user_order = current_user.user_orders.new(order_id: @order.id)
+    order = current_order
+    order_item = order.order_items.new(order_item_params)
+    if order.save
+      user_order = current_user.user_orders.new(order_id: order.id)
       current_user.order_items << order_item
-      @user_order.save
+      user_order.save
     end
-    session[:order_id] = @order.id
+    session[:order_id] = order.id
   end
 
   def update
@@ -22,7 +22,7 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
-    order_item = @order.order_items.find(params[:id])
+    order_item = current_order.order_items.find(params[:id])
     order_item.destroy
   end
 
@@ -32,12 +32,9 @@ class OrderItemsController < ApplicationController
     params.require(:order_item).permit(:quantity, :product_id)
   end
 
-  def set_current_order
-    @order = current_order
-  end
 
   def set_order_items
-    @order_items = @order.order_items
+    @order_items = current_order.order_items
   end
 
 end
