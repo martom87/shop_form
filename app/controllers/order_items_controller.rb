e0a64cc3743a13292b11225ecbd64e17e1732b7d
martom87 <marcintomala87@gmail.com>
2019-08-07 16:34:58 +0200

    Revert "wrong relation try"
    This reverts commit 3e456331bf8f9cd2a0886c02700eb7574b429eea.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,18 +1,47 @@
 class OrderItemsController < ApplicationController
 
+  before_action :set_order, only: %i[create update destroy]
+  before_action :set_order_item, only: %i[update destroy]
+  before_action :set_order_items, only: %i[update destroy]
+
+
   def create
-    OrderItems::CreateService.new(params, current_order, current_user, session).call
+    @order_item = @order.order_items.new(order_item_params)
+    @order.save
+    session[:order_id] = @order.id
     redirect_to request.referer
   end
 
   def update
-    OrderItems::UpdateService.new(params, current_order).call
+    @order_item.update_attributes(order_item_params)
     redirect_to request.referer
   end
 
   def destroy
-    OrderItems::DestroyService.new(params, current_order).call
+    @order_item.destroy
     redirect_to request.referer
   end
 
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :product_id)
+  end
+
+  def set_order
+    @order = current_order
+  end
+
+  def set_order_item
+    @order_item = @order.order_items.find(params[:id])
+  end
+
+  def set_order_item
+    @order_item = @order.order_items.find(params[:id])
+  end
+
+  def set_order_items
+    @order_items = @order.order_items
+  end
 end
